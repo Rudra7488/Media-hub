c60be88e5424890d8952d9553de5615ddb430320
Rudra Pratap Singh <rudrapratap7488@gmail.com>
2026-05-12 11:54:11 +0530

    ready

--- Backend/server.js
+++ Backend/server.js
@@ -78,8 +78,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`🚀 MediaDash Backend is successfully running on port ${PORT}!`);
 });
 
 export default app;
+
